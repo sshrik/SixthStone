e17f834c2d4eb59cf3c96d11e93925ffa5413e7a
최현준 <chj878194@naver.com>
2018-08-09 10:53:14 +0900

    프로토타입 정리

--- sixthStone.h
+++ sixthStone.h
@@ -51,6 +51,7 @@ int getCandidate(char plate[][PLATE_MAX], int candidateWeight[][PLATE_MAX], cord
 int getCandWeight(char plate[][PLATE_MAX], cord2D temp, int turn);
 void addWeight(char plate[][PLATE_MAX], cord2D temp, int cordWeight[][PLATE_MAX], int dir, int addNum);
 void changeCandWeight(char plate[][PLATE_MAX], int candidateWeight[][PLATE_MAX], cord2D before, int * weightList, int turn);
+int getLength(cord2D a, cord2D b);
 long long int calcWeight(int cordWeight[][PLATE_MAX]);
 int getWinState(char plate[][PLATE_MAX], cord2D *cord, int turn);
 int isWinState(char plate[][PLATE_MAX], cord2D cord, int turn, int dir, int continum);
